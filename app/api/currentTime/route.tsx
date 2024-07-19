dbfcd5061d5c8410eb147f4333f5bbe5233da037
KyawHtay <kyaw.htay@gmail.com>
2024-07-19 11:07:08 +0100

    Updated the server time and local time U.K

--- app/api/currentTime/route.tsx
+++ app/api/currentTime/route.tsx
@@ -3,9 +3,9 @@ import { NextResponse } from 'next/server';
 export const dynamic = 'force-dynamic';
 
 export async function GET() { 
-    const currentTime = new Date().toLocaleTimeString('en-US');
+    const currentTime = new Date().toLocaleTimeString('en-UK');
 
     return NextResponse.json({ 
-        message: `Hello from the API! The current time is ${currentTime}.`
+        message: `Hello from the API! The current time (U.K) is ${currentTime}.`
     });
 }
--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,11 @@ import { CurrentTimeFromAPI } from './components/CurrentTimeFromAPI';
 export default function Home() {
     noStore();
 
-    const timeOnServer = new Date().toLocaleTimeString('en-UK');
+    const timeOnServer = new Date().toLocaleTimeString('en-US');
     return(
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <div>
-                Hello Lewis Htay, This is a Next.js application hosted on Azure Static Web Apps with hybrid rendering. The time on the server is <strong>{timeOnServer}</strong>.
+                Hello Lewis Htay, This is a Next.js application hosted on Azure Static Web Apps with hybrid rendering. The time on the server (U.S) is <strong>{timeOnServer}</strong>.
             </div>
             <CurrentTimeFromAPI />
         </main>
